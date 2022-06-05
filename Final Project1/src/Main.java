49e9cd44b90adc4880f51455f1988cb8cd0ab128
aak2054 <65360279+aak2054@users.noreply.github.com>
2022-06-05 16:07:24 -0400

    Update Main.java

--- Final Project1/src/Main.java
+++ Final Project1/src/Main.java
@@ -2,11 +2,7 @@
 //This project is a STEM Quiz application that allows users to take a quiz with five questions from
 //either Science,Technology,Engineering or Mathematics subjects. The user will make a note of their choices, 
 //at the end the user will be able to check their answers base on the Correct Answers displayed for all questions. 
-//We went to the PQRC to get assistance
-// We used Oracle, GUI package and other resources found on Sakai
-//We got our STEM quiz questions from https://www.proprofs.com/quiz-school/story.php?title=geas-quiz ,
-//https://www.indiabix.com/general-knowledge/general-science/, https://www.q4quiz.com/math-quiz-questions-
-//answers-online-latest-math-quizzes-learn-mathematics/
+
 
 import javax.swing.JFrame;
 
